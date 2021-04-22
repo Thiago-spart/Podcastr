a036cf1d3354b7eacf4b20cb30c1a13f763e4d41
Thiago Spart <user000.000resu@gmail.com>
2021-04-22 13:39:07 -0300

    Aula 2
    Criação dos components header e player, integração com a fake API, método SSG, SASS e date-fns para setar a data

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+
+export default function Home(props) {
+  return (
+    <p>Index</p>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+  	props: {
+  		episodes: data,
+  	},
+  	revalidate: 60 * 60 * 8,
+  }
+}
+
+//SPA
+//SSR
+//SSG
